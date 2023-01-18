0339840f7dedf4f5ccb77ff288c3959998bba09f
khj <kim37171@cuk.edu>
2023-01-18 20:46:39 +0900

    feat: 채팅방 입/퇴장 이벤트 처리, 인원수 표시
    ChatRoomRepository.getUserCount -> 처음에 key값이 존재하지 않아서 null 반환, return 0
    ChatRoomRepository.plusUserCount -> 처음에 key값이 존재하지 않으면 매개변수로 입력받은 key값에 value를 0으로 세팅하고 increment 진행, return 1

--- src/main/java/com/gophagi/chatting/controller/ChatController.java
+++ src/main/java/com/gophagi/chatting/controller/ChatController.java
@@ -7,6 +7,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 import com.gophagi.chatting.dto.ChatMessage;
+import com.gophagi.chatting.repository.ChatRoomRepository;
+import com.gophagi.chatting.service.ChatService;
 import com.gophagi.chatting.service.JwtTokenProvider;
 
 import lombok.RequiredArgsConstructor;
@@ -15,9 +17,9 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class ChatController {
 
-	private final RedisTemplate redisTemplate;
-	private final ChannelTopic channelTopic;
 	private final JwtTokenProvider jwtTokenProvider;
+	private final ChatRoomRepository chatRoomRepository;
+	private final ChatService chatService;
 
 	/**
 	 * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
@@ -27,12 +29,9 @@ public class ChatController {
 		String nickname = jwtTokenProvider.getUserNameFromJwt(token);
 		// 로그인 회원 정보로 대화명 설정
 		message.setSender(nickname);
-		// 채팅방 입장시에는 대화명과 메시지를 자동으로 세팅한다.
-		if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
-			message.setSender("[알림]");
-			message.setMessage(nickname + "님이 입장하셨습니다.");
-		}
+		// 채팅방 인원수 세팅
+		message.setUserCount(chatRoomRepository.getUserCount(message.getRoomId()));
 		// Websocket에 발행된 메시지를 redis로 발행(publish)
-		redisTemplate.convertAndSend(channelTopic.getTopic(), message);
+		chatService.sendChatMessage(message);
 	}
 }
--- src/main/java/com/gophagi/chatting/service/ChatService.java
+++ src/main/java/com/gophagi/chatting/service/ChatService.java
@@ -1,29 +1,47 @@
 package com.gophagi.chatting.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gophagi.chatting.dto.ChatRoom;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+import com.gophagi.chatting.dto.ChatMessage;
+import com.gophagi.chatting.repository.ChatRoomRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class ChatService {
 
-    private final ObjectMapper objectMapper;
+	private final ChannelTopic channelTopic;
+	private final RedisTemplate redisTemplate;
+	private final ChatRoomRepository chatRoomRepository;
 
-    public <T> void sendMessage(WebSocketSession session, T message) {
-        try {
-            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
+	/**
+	 * destination정보에서 roomId 추출
+	 */
+	public String getRoomId(String destination) {
+		int lastIndex = destination.lastIndexOf('/');
+		if (lastIndex != -1)
+			return destination.substring(lastIndex + 1);
+		else
+			return "";
+	}
+
+	/**
+	 * 채팅방에 메시지 발송
+	 */
+	public void sendChatMessage(ChatMessage chatMessage) {
+		chatMessage.setUserCount(chatRoomRepository.getUserCount(chatMessage.getRoomId()));
+		if (ChatMessage.MessageType.ENTER.equals(chatMessage.getType())) {
+			chatMessage.setMessage(chatMessage.getSender() + "님이 방에 입장했습니다.");
+			chatMessage.setSender("[알림]");
+		} else if (ChatMessage.MessageType.QUIT.equals(chatMessage.getType())) {
+			chatMessage.setMessage(chatMessage.getSender() + "님이 방에서 나갔습니다.");
+			chatMessage.setSender("[알림]");
+		}
+		redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessage);
+	}
 }
